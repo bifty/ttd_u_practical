5d0d9eec9b66f1fddbd5790a9b5941391642446d
Sebastian Geiger <sngr@methodpark.de>
2019-03-05 13:22:21 +0100

    merged with course source

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,11 +2,20 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(int isbn) {
-		if (isbn == 373060291) {
+	public boolean checkISBN(String isbn) {
+		int total = 0;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			total += isbn.charAt(i) * (10 -i);
+		}
+		
+		if (total % 11 == 0) {
 			return true;
 		}
-		return false;
+		else {
+			return false;
+		}
 	}
 
 }
