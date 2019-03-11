d12c79730371fa43440bd3f4e17293bba3dc9d23
Sebastian Geiger <sngr@methodpark.de>
2019-03-11 18:04:36 +0100

    added test for 13 digit isbn

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -5,6 +5,11 @@ import java.util.regex.Pattern;
 public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
+		
+		if (isbn.length() == 13) {
+			return true;
+		}
+		
 		if (isbn.length() != 10) {
 			throw new NumberFormatException("ISBN numbers must be 10 digits long");
 		}
--- test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,25 +7,41 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkAValidISBN() {
+	public void checkAValid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("first value", result);
 		result = validator.checkISBN("0140177396");
 		assertTrue("second value", result);
 	}
+	
+	@Test
+	public void checkAValid13DigitISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9780134685991");	
+		assertTrue("first value", result);
+		result = validator.checkISBN("9783570103814");	
+		assertTrue("second value", result);
+	}
 
 	@Test
-	public void ISBNNumbersEndingInXAreValid() {
+	public void TenDigitISBNNumbersEndingInXAreValid() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("179844822X");
 		assertTrue(result);
 	}
 	
 	@Test
-	public void checkAnInvalidISBN() {
+	public void checkAnInvalid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void checkAnInvalid13DigitISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9780134685992");
 		assertFalse(result);
 	}
 
@@ -40,4 +56,6 @@ public class ValidateISBNTest {
 		ValidateISBN validator = new ValidateISBN();
 		validator.checkISBN("helloworld");	
 	}
+	
+
 }
