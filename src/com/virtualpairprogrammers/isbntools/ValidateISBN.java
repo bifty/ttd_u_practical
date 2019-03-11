c79431fb3101b21200513f0bd50aa1e95b4c7489
Sebastian Geiger <sngr@methodpark.de>
2019-03-11 18:23:14 +0100

    initial implemented 13 digit handling

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -7,8 +7,28 @@ public class ValidateISBN {
 	public boolean checkISBN(String isbn) {
 		
 		if (isbn.length() == 13) {
-			return true;
+			int total = 0;
+			
+			for (int i = 0; i < 13; i++) {
+				if (i % 2 == 0) {
+					total += Character.getNumericValue(isbn.charAt(i));
+			}
+				else {
+					total += Character.getNumericValue(isbn.charAt(i)) * 3;
+				}
+				
+			}
+			
+			if (total % 10 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+			
 		}
+		
+		else {
 		
 		if (isbn.length() != 10) {
 			throw new NumberFormatException("ISBN numbers must be 10 digits long");
@@ -37,6 +57,7 @@ public class ValidateISBN {
 		else {
 			return false;
 		}
+	}
 	}
 
 }
